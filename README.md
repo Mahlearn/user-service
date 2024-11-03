5edc6e31dfdc2ab686c9d873aa39d5429879a510
Eduardo Neira <edun327@gmail.com>
2024-11-03 17:52:57 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mahlearn - User Service
+
+User microservice application to handle user authentication and authorization
+
---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	java
+	war
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.session:spring-session-core")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
